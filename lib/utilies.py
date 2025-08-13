671960a505f5d237096f090b99fd47ba86210f24
Victorino Lavida <victorino.lavida@mtrade.mx>
2025-08-13 10:20:54 -0600

    feat: now in multiple mode quote feeds are send in both sides

--- lib/utilies.py
+++ lib/utilies.py
@@ -15,6 +15,15 @@ def generate_array_yield(base_yield:float, n:int=NUMBER_OF_QUOTES, direction="bi
     # for example [ 7.123, 7.113, 7.103, ..... ]
     # NOTE: if you need ask change the minus (-) sign for the plus sign
     if direction == "ask":
-        yiel_value = (base_yield - 0.1 * len(range(n)))
-        return [base_yield - i * 0.001 for i in range(n)]
+        yiel_value = (base_yield - 0.01 * (len(range(n)) + 1))
+        return [yiel_value - i * 0.001 for i in range(n)]
     return [(base_yield) + i * 0.001 for i in range(n)]
+
+
+def new_time_stamp() -> str:
+    from datetime import datetime, timezone
+
+    now = datetime.now(timezone.utc)
+    iso_timestamp = now.isoformat(timespec='milliseconds').replace('+00:00', 'Z')
+
+    return iso_timestamp
